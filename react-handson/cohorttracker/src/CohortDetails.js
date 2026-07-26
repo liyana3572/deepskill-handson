0cdce7352716b7fe5451b93c38e16ba9b2f8038e
liyana3572 <liyana3572@gmail.com>
2026-07-26 22:25:40 +0530

    Add react-handson project folder

---
+++ react-handson/cohorttracker/src/CohortDetails.js
@@ -0,0 +1,25 @@
+import styles from './CohortDetails.module.css';
+
+function CohortDetails(props) {
+    const { cohortCode, technology, startDate, currentStatus, coachName, trainerName } = props.cohort;
+
+    return (
+        <div className={styles.box}>
+            <h3 style={{ color: currentStatus === 'Ongoing' ? 'green' : 'blue' }}>
+                {cohortCode} - <span>{technology}</span>
+            </h3>
+            <dl>
+                <dt>Started On</dt>
+                <dd>{startDate}</dd>
+                <dt>Current Status</dt>
+                <dd>{currentStatus}</dd>
+                <dt>Coach</dt>
+                <dd>{coachName}</dd>
+                <dt>Trainer</dt>
+                <dd>{trainerName}</dd>
+            </dl>
+        </div>
+    );
+}
+
+export default CohortDetails;
